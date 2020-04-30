74444e4a648fdce543624217690a4bb407248ee4
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2020-04-30 13:14:57 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
+
+    get "/" do
+        erb :user_input
+    end
+
+    post "/piglatinize" do
+        @piggy = PigLatinizer.new
+
+        erb :results 
+    end
 end
+
