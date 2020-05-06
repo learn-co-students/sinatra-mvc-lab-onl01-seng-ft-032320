347570368ccf742778472ee860ec897461b1fcc8
github username <github email address>
2020-05-06 09:20:26 -0400

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,28 @@
+class PigLatinizer
+    attr_reader :words
+  
+
+    def piglatinize(words)
+        return "#{words}way" if(words.length == 1)
+        words_array = words.split(" ")
+        new_words = words_array.collect do |word|
+          self.turn_word_to_pig_latin(word)
+        end
+        new_words.join(" ")
+      end
+      def turn_word_to_pig_latin(word)
+        first_letter = word[0]
+        rest_of_word = word[1..]
+        vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+        consonants = [first_letter]
+        return "#{word}way" if(vowels.include?(first_letter))
+        rest_of_word.split("").each do |letter|
+          if(vowels.include?(letter))
+            return "#{word[word.index(letter)..] + consonants.join + "ay"}"
+          else
+            consonants << letter
+          end
+        end
+      end
+
+end
